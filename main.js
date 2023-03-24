9e82f6c479a416eb91b171d60dde5ee063374fb8
HGlab <54232526+HGlab01@users.noreply.github.com>
2023-03-24 17:26:01 +0100

    spread API calls + delete obsolete files

--- main.js
+++ main.js
@@ -60,6 +60,10 @@ class ApgInfo extends utils.Adapter {
             this.log.debug('Internet connection detected. Everything fine!');
         }
 
+        const delay = Math.floor(Math.random() * 30000);
+        this.log.info(`Delay execution by ${delay}ms to better spread API calls`);
+        await this.sleep(delay);
+
         await JsonExplorer.setLastStartTime();
         let resultPeakHours = await this.ExecuteRequestPeakHours();
         let resultDayAhead = await this.ExecuteRequestDayAhead();
@@ -78,10 +82,15 @@ class ApgInfo extends utils.Adapter {
     onUnload(callback) {
         try {
             this.log.info('cleaned everything up...');
+            this.unloaded = true;
             callback();
         } catch (e) {
             callback();
         }
+    }
+
+    sleep(ms) {
+        return new Promise(resolve => setTimeout(() => !this.unloaded && resolve(), ms));
     }
 
     /*
@@ -233,7 +242,7 @@ class ApgInfo extends utils.Adapter {
             let day3 = new Date(day0.getTime() + oneDayTime * 3);
             let day4 = new Date(day0.getTime() + oneDayTime * 4);
 
-            let jDay0 = {}, jDay1 = {}, jDay2 = {}, jDay3 = {},jDay4 = {},jDayAll = {};
+            let jDay0 = {}, jDay1 = {}, jDay2 = {}, jDay3 = {}, jDay4 = {}, jDayAll = {};
             let iHour = 0;
             let sHour = '';
             let i = 1;
